b6e6aa8c357a1af2867334df404cb9fd748d996f
John Nieri <niericentral@gmail.com>
2013-05-14 01:16:42 +0900

    changed from __init__ specifying results to just unit test type test that it calls _standardize for both image and mask and _mask for the combination.

--- tests/integration/test_base.py
+++ tests/integration/test_base.py
@@ -1,5 +1,6 @@
 import unittest
 
+from mock import patch
 import numpy
 
 from imagefinder import ImageFinder
@@ -7,22 +8,15 @@ from imagefinder import ImageFinder
 
 class Test_ImageFinder(unittest.TestCase):
     # Initialization
-    def test___init___raises_TypeError_unless_numpy_bgr_bgra_or_gray(self):
-        bgr = generic_image(channels=3)
-        bgra = generic_image(channels=4)
-        gray = generic_image(channels=1)
-        bad = generic_image(channels=2)
-        totally_wrong = 'whut'
-        # confirm the bad one fails
-        self.assertRaises(TypeError, ImageFinder, bad)
-        self.assertRaises(TypeError, ImageFinder, totally_wrong)
-        # confirm the good ones don't fail
-        for ok_img in (bgr, bgra, gray):
-            try:
-                ImageFinder(ok_img)
-            except Exception as e:
-                self.fail('Unexpectedly failed to initialize with'
-                          ' a valid image:\n{}.'.format(e))
+    @patch.object(ImageFinder, '_standardize')
+    def test___init___standardizes_image_and_mask(self, m_standardize):
+        m_standardize.return_value = generic_image(channels=3)  # some valid
+        img = generic_image()
+        mask = generic_image()
+        ImageFinder(img, mask=mask)
+        # self.assertTrue(m_standardize.called)
+        m_standardize.assert_called_first_with(img)
+        m_standardize.assert_called_second_with(mask)
 
     def test___init___stores_base_image_as_template_if_no_sizes_provided(self):
         h, w = size_key = 10, 10
@@ -30,30 +24,14 @@ class Test_ImageFinder(unittest.TestCase):
         imgf = ImageFinder(img)
         self.assertIs(imgf._templates[size_key], imgf._base)
 
-    def test___init___stores_image_internally_as_bgr(self):
-        bgra_channels = 4
-        imgf = generic_ImageFinder(channels=bgra_channels)
-        internal_image = imgf._base
-        bgr_channels = 3
-        self.assertEqual(internal_image.shape[2], bgr_channels)
-
-    def test___init___applies_an_optional_mask_to_internal_image(self):
-        h, w = 10, 10
-        # make a special white image with values over random range
-        high_value = 1000
-        white_img = numpy.ndarray((h, w), dtype=numpy.uint16)
-        white_img.fill(high_value)
-        # make a mask with zeros at specified positions
-        mask_img = numpy.ndarray((h, w), dtype=numpy.uint8)
-        mask_img.fill(255)
-        masked_positions = [(0, 0), (5, 5)]
-        for p in masked_positions:
-            mask_img[p] = 0  # 0 ==> mask
-        # make the image finder and confirm the internal image was masked
-        imgf = ImageFinder(white_img, mask=mask_img)
-        for p in masked_positions:
-            masked_pixel = imgf._base[p]
-            self.assertFalse(numpy.all(masked_pixel == high_value))
+    @patch.object(ImageFinder, '_mask')
+    def test___init___applies_an_optional_mask_to_image(self, m_mask):
+        m_mask.return_value = generic_image(channels=3)  # some valid
+        img = generic_image()
+        mask = generic_image()
+        ImageFinder(img, mask=mask)
+        # self.assertTrue(m_standardize.called)
+        m_mask.assert_called()
 
     # Attributes and Sizing
     def test_internal_template_dict_keys_are_int_tuples_of_height_width(self):
@@ -72,9 +50,40 @@ class Test_ImageFinder(unittest.TestCase):
     # Searching for templates:
     def test_locate_returns_l_after_all_comparisons_if_over_min_threshold(self):
         raise NotImplementedError
+    # Internal specifications
+    def test__standardize_raises_TypeError_unless_bgr_bgra_or_gray(self):
+        bgr = generic_image(channels=3)
+        bgra = generic_image(channels=4)
+        gray = generic_image(channels=None)
+        unhandled_channel_count = generic_image(channels=2)
+        just_a_string = 'whut'
+        # confirm the bad one fails
+        imgf = generic_ImageFinder()
+        self.assertRaises(TypeError,
+                          imgf._standardize, unhandled_channel_count)
+        self.assertRaises(TypeError,
+                          imgf._standardize, just_a_string)
+        # confirm the valid ones don't fail
+        for ok_img in (bgr, bgra, gray):
+            imgf._standardize(ok_img)
 
-    def test_locate_returns_l_immediately_if_over_immediate_threshold(self):
-        raise NotImplementedError
+    def test__mask_puts_noise_on_masked_locations(self):
+        size = h, w = 10, 10
+        # make a special white image with values over random range
+        high_value = 1000
+        white = numpy.ndarray((h, w), dtype=numpy.uint16)
+        white.fill(high_value)
+        # make a mask with zeros at specified positions
+        mask = numpy.ndarray((h, w), dtype=numpy.uint8)
+        mask.fill(255)
+        masked_positions = [(0, 0), (5, 5)]
+        for p in masked_positions:
+            mask[p] = 0
+        # make the image finder and confirm the internal image was masked
+        imgf = ImageFinder(white, mask=mask, sizes=None)
+        for p in masked_positions:
+            masked_pixel = imgf._templates[size][p]
+            self.assertFalse(numpy.all(masked_pixel == high_value))
 
 
 def generic_ImageFinder(width=None, height=None, channels=None, sizes=None):
