5cf6a02d0dd12b2243239d1d9fb84f66f7dbd99b
John Nieri <niericentral@gmail.com>
2013-05-14 23:48:01 +0900

    version bump to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='ImageFinder',
-    version='0.1.0',
+    version='0.1.1',
     packages=['imagefinder'],
     requires=['cv2', 'numpy'],
     tests_require=['cv2', 'mock', 'numpy'],
