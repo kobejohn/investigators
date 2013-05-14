ff2d0539e04467529843d2824172c0adedfa4039
John Nieri <niericentral@gmail.com>
2013-05-14 23:47:05 +0900

    update setup.py to match latest tests, readme.

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     version='0.1.0',
     packages=['imagefinder'],
     requires=['cv2', 'numpy'],
-    tests_require=['mock, numpy'],
+    tests_require=['cv2', 'mock', 'numpy'],
     url='http://github.com/kobejohn/ImageFinder',
     license='MIT',
     author='KobeJohn',
     author_email='niericentral@gmail.com',
-    description='Repeatedly find a template within scenes.'
+    description='Find one image within another.'
 )
