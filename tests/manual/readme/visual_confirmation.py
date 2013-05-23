24d06c2d63ff59c75a8311225d27d11dda60b672
John Nieri <niericentral@gmail.com>
2013-05-24 06:53:20 +0900

    Updated readme and readme test code to be more readable and include new TankLevel investigator.

--- tests/manual/readme/visual_confirmation.py
+++ tests/manual/readme/visual_confirmation.py
@@ -1,57 +1,75 @@
+import numpy
 from investigators.visuals import cv2  # package includes a fallback cv2
 from investigators.visuals import TemplateFinder, ProportionalRegion, Grid
-from investigators.visuals import ImageIdentifier
+from investigators.visuals import ImageIdentifier, TankLevel
 
 # Show the starting screenshot
 screenshot = cv2.imread('screenshot 1280x800, game 800x600.png')
 cv2.imshow('screenshot', screenshot)
+cv2.waitKey()
 
 # Use TemplateFinder to locate and show an image of the game on the screen
-game_template = cv2.imread('game 640x480.png')
-game_mask = cv2.imread('game mask 640x480.png')
-game_possible_sizes = (600, 800), (240, 320)
-imgf = TemplateFinder(game_template, sizes=game_possible_sizes, mask=game_mask,
-                      acceptable_threshold=0.5,
-                      immediate_threshold=0.1)
-game_top, game_left, game_bottom, game_right = imgf.locate_in(screenshot)
-game = screenshot[game_top:game_bottom, game_left:game_right]
-cv2.waitKey()
+template = cv2.imread('game 640x480.png')
+mask = cv2.imread('game mask 640x480.png')
+possible_sizes = (600, 800), (240, 320)
+finder = TemplateFinder(template, sizes=possible_sizes, mask=mask)
+t, l, b, r = finder.locate_in(screenshot)
+game = screenshot[t:b, l:r]
+cv2.destroyWindow('screenshot')
 cv2.imshow('extracted game', game)
+cv2.waitKey()
 
 # Use ProportionalRegion to isolate the board within the game
-board_top_proportion = float(80) / 480  # these are just from inspection
-board_left_proportion = float(135) / 640
-board_bottom_proportion = float(450) / 480
-board_right_proportion = float(504) / 640
-pr = ProportionalRegion((board_top_proportion, board_left_proportion,
-                        board_bottom_proportion, board_right_proportion))
-board_top, board_left, board_bottom, board_right = pr.region_in(game)
-board = game[board_top:board_bottom, board_left:board_right]
-cv2.waitKey()
+tlbr_proportions = (80.0 / 480, 135.0 / 640, 450.0 / 480, 504.0 / 640)
+proportional = ProportionalRegion(tlbr_proportions)
+t, l, b, r = proportional.region_in(game)
+board = game[t:b, l:r]
 cv2.imshow('extracted board', board)
+cv2.waitKey()
 
 # Use Grid to split the grid into image cells. record only the first four
 board_dimensions = 8, 8
-tile_padding = (0.1, 0.1, 0.1, 0.1)
+tile_padding = (0.1, 0.1, 0.1, 0.1)  # just for demonstration
 grid = Grid(board_dimensions, tile_padding)
 first_four_tiles = list()
 for grid_p, cell_borders in grid.borders_by_grid_position(board):
     if grid_p in ((0, 0), (0, 1), (0, 2), (0, 3)):
-        top, left, bottom, right = (cell_borders.top, cell_borders.left,
-                                    cell_borders.bottom, cell_borders.right)
-        first_four_tiles.append(board[top:bottom, left:right].copy())
+        t, l, b, r = cell_borders
+        first_four_tiles.append(board[t:b, l:r])
 
 # Use ImageIdentifier to identify each of the first four tiles
-ii = ImageIdentifier({'red': cv2.imread('r.png'),
-                      'blue': cv2.imread('b.png'),
-                      'coins': cv2.imread('m.png'),
-                      'skull': cv2.imread('s.png')})
+identifier = ImageIdentifier({'red': cv2.imread('r.png'),
+                              'blue': cv2.imread('b.png'),
+                              'coins': cv2.imread('m.png'),
+                              'skull': cv2.imread('s.png')})
 for tile in first_four_tiles:
-    name = ii.identify(tile)
+    name = identifier.identify(tile)
     cv2.putText(tile, name, (2, 20), 0, 0.6, (255, 255, 255), 2)
-    cv2.waitKey()
     cv2.imshow('identified original tile', tile)
+    cv2.waitKey()
+cv2.destroyWindow('identified original tile')
+cv2.destroyWindow('extracted board')
+
+# Use ProportionalRegion to isolate the player's health within the game
+tlbr_proportions = (76.0 / 480, 8.0 / 640, 87.0 / 480, 116.0 / 640)
+proportional = ProportionalRegion(tlbr_proportions)
+t, l, b, r = proportional.region_in(game)
+health = game[t:b, l:r]
+
+# Use TankLevel to measure how full the health bar is
+health_color = (4, 4, 110)
+empty_health_color = (15, 15, 50)
+ignore_color = (0, 0, 0)
+tank_level = TankLevel(health_color, empty_health_color, ignore_color)
+health = numpy.rot90(health)  # make it upright for the TankLevel measurement
+health_pcnt = 100.0 * tank_level.how_full(health)
+health_string = '~{:2.0f}%'.format(health_pcnt)
+health = numpy.rot90(health, -1)  # rotate and enlarge for display
+h, w = health.shape[0:2]
+health = cv2.resize(health, (w * 3, h * 3), interpolation=cv2.INTER_CUBIC)
+cv2.putText(health, health_string, (25, 25), 0, 0.8, (0, 255, 0), 2)
+cv2.imshow('player health bar', health)
+cv2.waitKey()
 
 # clean up
-cv2.waitKey()
 cv2.destroyAllWindows()
